0347a8b1ecf06a92acd1530909a845eb18da0c62
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-11-12 13:38:04 -0500

    initial commit

---
+++ StudentRecords/js/students.js
@@ -0,0 +1,63 @@
+var students = [{
+        name: 'Robert',
+        track: 'JavaScript',
+        achievements: 4,
+        points: 556
+    },
+    {
+        name: 'Maggie',
+        track: 'Front End',
+        achievements: 14,
+        points: 1097
+    },
+    {
+        name: 'Joel',
+        track: 'Ruby',
+        achievements: 6,
+        points: 356
+    },
+    {
+        name: 'Hans',
+        track: 'iOS',
+        achievements: 12,
+        points: 2556
+    },
+    {
+        name: 'Ashley',
+        track: 'HTML',
+        achievements: 3,
+        points: 956
+    }
+];
+
+var student;
+var message = '';
+var search;
+
+function print(message) {
+    var div = document.getElementById('output');
+    div.innerHTML = message;
+    //document.write(message);
+}
+
+function getStudentReport(student) {
+    var report = '<h2>Students name: ' + student.name + '<h2>';
+    report += '<p>Track: ' + student.track + '</p>';
+    report += '<p>Achievements: ' + student.achievements + '</p>';
+    report += '<p>Points: ' + student.points + '</p>';
+    return report;
+}
+
+while (true) {
+    search = prompt('Search for records. Type list to show all, quit to exit.')
+    if (search === null || search.toLowerCase() === 'quit') {
+        break;
+    }
+    for (var i = 0; i < students.length; i++) {
+        student = students[i];
+        if (student.name === search) {
+            message = getStudentReport(student);
+            print(message);
+        }
+    }
+}
