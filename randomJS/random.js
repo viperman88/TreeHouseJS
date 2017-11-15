2f753161c52bddf188cad9b17b3edb959b7f94bf
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-11-14 20:37:36 -0500

    updated app

--- randomJS/random.js
+++ randomJS/random.js
@@ -11,3 +11,16 @@ const randNumBetween = (low, high) => {
 }
 randNumBetween(12, 38);
 randNumBetween(12, 'hello');
+
+/*
+Alternate random number app
+
+let userInput = prompt('Pick a starting number!');
+let first = parseInt(userInput);
+let userInput2 = prompt('Pick a number, any number!');
+let second = parseInt(userInput2);
+//finds the difference between start and end num, and adds start num to start at where difference is used to create spread
+let randomNum = Math.floor(Math.random() * (second - first + 1)) + first;
+let message = '<h1>A random number was generated between ' + first + ' and ' + second + ', that number is ' + randomNum + '</h1>';
+document.write(message);
+*/
