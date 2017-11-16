32ed2ad0b00d247e2175f5f3cfb1512dcf59b648
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-11-16 01:57:25 -0500

    updated app with new features

--- DOM_JS/app.js
+++ DOM_JS/app.js
@@ -6,27 +6,67 @@ const descriptionButton = document.querySelector('button.description');
 const listUl = listDiv.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
-
+const lis = listUl.children;
 //Remove all commented code on this page and index.html to add seperate event hadling
 //const removeItemButton = document.querySelector('button.removeItemButton');
 
-listDiv.addEventListener('mouseover', (event) => {
-    if (event.target.tagName == 'LI') {
-        event.target.textContent = event.target.textContent.toUpperCase();
-    }
-});
+// listDiv.addEventListener('mouseover', (event) => {
+//     if (event.target.tagName == 'LI') {
+//         event.target.textContent = event.target.textContent.toUpperCase();
+//     }
+// });
+//
+// listDiv.addEventListener('mouseout', (event) => {
+//     if (event.target.tagName == 'LI') {
+//         event.target.textContent = event.target.textContent.toLowerCase();
+//     }
+// });
 
-listDiv.addEventListener('mouseout', (event) => {
-    if (event.target.tagName == 'LI') {
-        event.target.textContent = event.target.textContent.toLowerCase();
-    }
-});
+function attachListItemButtons(li) {
+    let up = document.createElement('button');
+    up.className = 'up';
+    up.textContent = 'Up';
+    li.appendChild(up);
+
+    let down = document.createElement('button');
+    down.className = 'down';
+    down.textContent = 'Down';
+    li.appendChild(down);
+
+    let remove = document.createElement('button');
+    remove.className = 'remove';
+    remove.textContent = 'Remove';
+    li.appendChild(remove);
+}
+for (let i = 0; i < lis.length; i++) {
+    attachListItemButtons(lis[i]);
+}
 
 listUl.addEventListener('click', (event) => {
-    if (event.target.tagName == 'LI') {
+    if (event.target.tagName == 'BUTTON') {
+        if (event.target.className == 'remove') {
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+        }
+    }
+    if (event.target.className == 'up') {
         let li = event.target.parentNode;
+        let prevLi = li.previousElementSibling;
         let ul = li.parentNode;
-        ul.removeChild(li);
+        if (prevLi) {
+            ul.insertBefore(li, prevLi)
+        }
+    }
+    //Due to no insertAfter method, added nextElementSibling and switched
+    //arguments in insertBefore() to allow elements to move after
+    if (event.target.className == 'down') {
+        let li = event.target.parentNode;
+        let nextLi = li.nextElementSibling;
+        let ul = li.parentNode;
+        if (nextLi) {
+            ul.insertBefore(nextLi, li)
+        }
     }
 });
 
@@ -49,6 +89,7 @@ addItemButton.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
     li.textContent = addItemInput.value;
+    attachListItemButtons(li);
     ul.appendChild(li);
     addItemInput.value = '';
 });
