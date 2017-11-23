c995390ff06de926a1c8c427188308536f2df2cd
Robert Amato <robertamato@Roberts-MBP.fios-router.home>
2017-11-22 21:10:58 -0500

    added media/movie sections

--- TreeTunes/app.js
+++ TreeTunes/app.js
@@ -3,25 +3,29 @@ var playlist = new Playlist();
 var rodeo = new Song('Rodeo', 'Garth Brooks', '4:01');
 var amarilloByMorning = new Song('Amarillo by Morning', 'George Straight', '3:39');
 
+var aBronxTale = new Movie('A Bronx Tale', 2003, '2:23:19');
+
 playlist.add(rodeo);
 playlist.add(amarilloByMorning);
+
+playlist.add(aBronxTale);
 
 var playListElement = document.getElementById('playlist');
 
 playlist.renderInElement(playListElement);
 
 var playButton = document.getElementById('play');
-  playButton.onclick = function() {
-  playlist.play();
-  playlist.renderInElement(playListElement);
+playButton.onclick = function() {
+    playlist.play();
+    playlist.renderInElement(playListElement);
 }
 var nextButton = document.getElementById('next');
-  nextButton.onclick = function() {
-  playlist.next();
-  playlist.renderInElement(playListElement);
+nextButton.onclick = function() {
+    playlist.next();
+    playlist.renderInElement(playListElement);
 }
 var stopButton = document.getElementById('stop');
-  stopButton.onclick = function() {
-  playlist.stop();
-  playlist.renderInElement(playListElement);
+stopButton.onclick = function() {
+    playlist.stop();
+    playlist.renderInElement(playListElement);
 }
